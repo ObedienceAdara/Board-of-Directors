46ae9dc4229de2ffac3c0ded0edd512ff595aab5
Obedience Adara <166041189+ObedienceAdara@users.noreply.github.com>
2026-09-05 10:14:24 +0100

    feat: export structured search provenance

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,9 +2,9 @@
 
 from .notion import create_notion_board, create_notion_page
 from .pdf import generate_pdf
-from .search import get_search_tool, multi_search, parse_search_results
+from .search import get_search_tool, multi_search, multi_search_with_provenance, parse_search_results, search_with_provenance
 
 __all__ = [
     "create_notion_board", "create_notion_page", "generate_pdf",
-    "get_search_tool", "multi_search", "parse_search_results",
+    "get_search_tool", "multi_search", "multi_search_with_provenance", "parse_search_results", "search_with_provenance",
 ]
